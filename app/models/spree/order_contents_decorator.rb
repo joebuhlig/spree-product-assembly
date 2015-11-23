27b9f33c1f6a4bb5d738a9bf0e4d5d8766f0ba9b
Joe Buhlig <joe@joebuhlig.com>
2015-11-23 06:50:28 -0600

    Removed conflicting decorator method

--- app/models/spree/order_contents_decorator.rb
+++ app/models/spree/order_contents_decorator.rb
@@ -1,25 +1,25 @@
 module Spree
   OrderContents.class_eval do
 
-    def add_to_line_item(variant, quantity, options = {})
-      line_item = grab_line_item_by_variant(variant, false, options)
+    # def add_to_line_item(variant, quantity, options = {})
+    #   line_item = grab_line_item_by_variant(variant, false, options)
 
-      if line_item && part_variants_match?(line_item, variant, quantity, options)
-        line_item.quantity += quantity.to_i
-        line_item.currency = currency unless currency.nil?
-      else
-        opts = { currency: order.currency }.merge ActionController::Parameters.new(options).
-                                            permit(PermittedAttributes.line_item_attributes)
-        line_item = order.line_items.new(quantity: quantity,
-                                          variant: variant,
-                                          options: opts)
-      end
+    #   if line_item && part_variants_match?(line_item, variant, quantity, options)
+    #     line_item.quantity += quantity.to_i
+    #     line_item.currency = currency unless currency.nil?
+    #   else
+    #     opts = { currency: order.currency }.merge ActionController::Parameters.new(options).
+    #                                         permit(PermittedAttributes.line_item_attributes)
+    #     line_item = order.line_items.new(quantity: quantity,
+    #                                       variant: variant,
+    #                                       options: opts)
+    #   end
 
-      line_item.target_shipment = options[:shipment] if options.has_key? :shipment
-      line_item.save!
+    #   line_item.target_shipment = options[:shipment] if options.has_key? :shipment
+    #   line_item.save!
 
-      line_item
-    end
+    #   line_item
+    # end
 
     def add_to_line_item_with_parts(variant, quantity, options = {})
       add_to_line_item_without_parts(variant, quantity, options).
